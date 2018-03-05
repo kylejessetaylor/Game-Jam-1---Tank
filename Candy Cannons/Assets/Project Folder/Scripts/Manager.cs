4b411ff1f5a0067e193a6ec4b1a345970e3b43ce
Kyle Taylor <kyle.jesse.taylor@gmail.com>
2018-03-05 22:51:41 +1100

    Finished Spawners.cs  - MineSpawner  - PowerUpSpawner
    Updated Manager.cs

--- Candy Cannons/Assets/Project Folder/Scripts/Manager.cs
+++ Candy Cannons/Assets/Project Folder/Scripts/Manager.cs
@@ -4,29 +4,6 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    [Header("Spawner")]
-    #region SpawnerVariables
-    //Time till first spawn
-    public float firstSpawn;
-    //Time between spawning
-    private float cycleSpawnTime;
-    //Range time 1
-    public float cycleTime1;
-    //Range time 2
-    public float cycleTime2;
-
-    //List of all pre-placed spawn locations
-    public List<GameObject> spawnLocations;
-    //Records last spawn location used
-    private int lastSpawnLocation;
-
-    //List of all power-up prefabs
-    public List<GameObject> powerUps;
-    //Records last power up that spawned
-    private int lastPowerUp;
-
-    #endregion
-
     [Header("MenuInterface")]
     #region MenuVariables
     //Main Menu "Scene"
@@ -40,8 +17,7 @@ public class Manager : MonoBehaviour
 // Use this for initialization
 void Start()
     {
-        //Sets Invoke Repeating
-        SpawnerStart();
+        
     }
 
     // Update is called once per frame
@@ -50,53 +26,6 @@ void Start()
 
     }
 
-    #region SpawnerFunctions
-    private void SpawnerStart()
-    {
-        //Sets first cycleSpawnTime
-        cycleSpawnTime = FloatTime();
-        //Spawner for Power Ups
-        InvokeRepeating("SpawnPowerUp", firstSpawn, cycleSpawnTime);
-    }
-
-    public void SpawnPowerUp()
-    {
-        //Picks random Power up from list
-        int currentPowerUp = Random.Range(0, powerUps.Count);
-        GameObject powerUp = powerUps[currentPowerUp];
-
-
-
-        //Picks random Location from the list of spawn locations
-        int spotToSpawn = Random.Range(0, spawnLocations.Count);
-        GameObject spawnLocation = spawnLocations[spotToSpawn];
-
-        //Prevents duplicate spawn locations & power Ups
-        if (lastPowerUp == currentPowerUp || lastSpawnLocation == spotToSpawn)
-        {
-            SpawnPowerUp();
-        }
-        //Spawns Power Up on location
-        else
-        {
-            //Instantiates Power Up & places to Spawn Location
-            GameObject powerSpawned = Instantiate(powerUp);
-            powerSpawned.transform.position = spawnLocation.transform.position;
-
-            //Records new Last power up & spawn location
-            lastPowerUp = currentPowerUp;
-            lastSpawnLocation = spotToSpawn;
-
-            //Changes cycleSpawnTime
-            cycleSpawnTime = FloatTime();
-        }
-    }
-
-    public float FloatTime()
-    {
-        float newTime = Random.Range(cycleTime1, cycleTime2);
-        return (newTime);
-    }
-    #endregion
+    
 
 }
---
+++ Candy Cannons/Assets/Project Folder/Scripts/Spawners.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawners : MonoBehaviour {
+
+    [Header("Power Up Spawner")]
+    #region SpawnerPowerUpVariables
+    //Time till first spawn
+    public float powerUpFirstSpawn;
+    //Time between spawning
+    private float cyclePowerUPSpawnTime;
+    //Range time 1
+    public float powerUpTimeMin;
+    //Range time 2
+    public float powerUpTimeMax;
+
+    //List of all pre-placed spawn locations
+    public List<GameObject> powerUpSpawnLocations;
+    //Records last spawn location used
+    private int lastPowerUpSpawnLocation;
+
+    //List of all power-up prefabs
+    public List<GameObject> powerUps;
+    //Records last power up that spawned
+    private int lastPowerUp;
+
+    #endregion
+
+    [Header("Mine Spawner")]
+    #region SpawnerPowerUpVariables
+    //Time till first spawn
+    public float mineFirstSpawn;
+    //Time between spawning
+    private float cycleMineSpawnTime;
+    //Range time 1
+    public float mineTimeMin;
+    //Range time 2
+    public float mineTimeMax;
+
+    //List of all pre-placed spawn locations
+    public List<GameObject> mineSpawnLocations;
+
+    //Mine GameObject
+    public GameObject mine;
+
+    #endregion
+
+    void Start () {
+        //Sets Invoke Repeating for Spawner Power Ups
+        SpawnerPowerUpStart();
+        //Sets Inkove Repeating for Spawner Mines
+        SpawnerMineStart();
+    }
+
+    #region SpawnerPowerUpFunctions
+    private void SpawnerPowerUpStart()
+    {
+        //Sets first cycleSpawnTime
+        cyclePowerUPSpawnTime = FloatTime(powerUpTimeMin, powerUpTimeMax);
+        //Spawner for Power Ups
+        InvokeRepeating("SpawnPowerUp", powerUpFirstSpawn, cyclePowerUPSpawnTime);
+    }
+
+    public void SpawnPowerUp()
+    {
+        //Picks random Power up from list
+        int currentPowerUp = Random.Range(0, powerUps.Count);
+        GameObject powerUp = powerUps[currentPowerUp];
+
+
+
+        //Picks random Location from the list of spawn locations
+        int spotToSpawn = Random.Range(0, powerUpSpawnLocations.Count);
+        GameObject spawnLocation = powerUpSpawnLocations[spotToSpawn];
+
+        //Prevents duplicate spawn locations & power Ups
+        if (lastPowerUp == currentPowerUp || lastPowerUpSpawnLocation == spotToSpawn || AlreadyASpawnHere(spawnLocation, "PowerUpSpawner") == true)
+        {
+            SpawnPowerUp();
+        }
+        //Spawns Power Up on location
+        else
+        {
+            //Checks if all spawn locations have a power up on them
+            if (GameObject.FindGameObjectsWithTag("PowerUpSpawner").Length <= GameObject.FindGameObjectsWithTag("PowerUp").Length)
+            {
+                return;
+            }
+            //Checks that another Power Up isn't here either
+            else if (spawnLocation.transform.childCount != 0)
+            {
+                SpawnPowerUp();
+            }
+            else
+            {
+                //Instantiates Power Up & places to Spawn Location
+                GameObject powerSpawned = Instantiate(powerUp, spawnLocation.transform);
+                powerSpawned.transform.position = spawnLocation.transform.position;
+
+                //Records new Last power up & spawn location
+                lastPowerUp = currentPowerUp;
+                lastPowerUpSpawnLocation = spotToSpawn;
+
+                //Changes cycleSpawnTime
+                cyclePowerUPSpawnTime = FloatTime(powerUpTimeMin, powerUpTimeMax);
+            }            
+        }
+    }
+
+    #endregion
+
+
+    #region SpawnerMineFunctions
+    private void SpawnerMineStart()
+    {
+        //Sets first cycleSpawnTime
+        cycleMineSpawnTime = FloatTime(mineTimeMin, mineTimeMax);
+        //Spawner for Power Ups
+        InvokeRepeating("SpawnMine", mineFirstSpawn, cycleMineSpawnTime);
+    }
+
+    public void SpawnMine()
+    {
+        if (mineSpawnLocations.Count == 0)
+        {
+            CancelInvoke("SpawnMine");
+            return;
+        }
+
+        //Picks random Location from the list of spawn locations
+        int spotToSpawn = Random.Range(0, mineSpawnLocations.Count);
+        GameObject spawnLocation = mineSpawnLocations[spotToSpawn];
+                
+        //Checks if something is already in that spot
+        if (AlreadyASpawnHere(spawnLocation, "Mine") == true)
+        {
+            SpawnMine();
+        }
+
+        //Spawns Mine on location
+        else
+        {
+            //Instantiates Mine & places to Spawn Location
+            GameObject mineSpawned = Instantiate(mine, spawnLocation.transform);
+            mineSpawned.transform.position = spawnLocation.transform.position;
+            //Removes from list, reventing multiple spawns on 1 location
+            mineSpawnLocations.Remove(spawnLocation);
+
+            //Changes cycleSpawnTime
+            cycleMineSpawnTime = FloatTime(mineTimeMin, mineTimeMax);
+        }
+
+    }
+
+    #endregion
+
+    //Checks whether power up has already spawned here with this string as a tag
+    public bool AlreadyASpawnHere(GameObject mySpawnLocation, string GameObjectTag)
+    {
+        //Closest Power Up to Spawn Location
+        float distanceToClosestPowerUp = Mathf.Infinity;
+        GameObject closestPowerUp = null;
+        foreach (GameObject powerUp in GameObject.FindGameObjectsWithTag(GameObjectTag))
+        {
+            float distanceToPowerUp = Vector3.Distance(transform.position, mySpawnLocation.transform.position);
+            if (distanceToPowerUp < distanceToClosestPowerUp)
+            {
+                distanceToClosestPowerUp = distanceToPowerUp;
+                closestPowerUp = powerUp;
+            }
+        }
+
+        //Power Up here
+        if (distanceToClosestPowerUp <= 0.05f)
+        {
+            return (true);
+        }
+        //No Power Up here
+        else
+        {
+            return (false);
+        }
+
+    }
+
+    //Gives a random float from between a range
+    public float FloatTime(float min, float max)
+    {
+        float newTime = Random.Range(min, max);
+        return (newTime);
+    }
+}
