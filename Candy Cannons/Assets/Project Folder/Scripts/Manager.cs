3bff70810157ba9af3b355a8dee4dbf87b68a893
ryan0432 <ryan0432@gmail.com>
2018-03-06 17:06:22 +1100

    UIManager done. Canvas prefab updated.

--- Candy Cannons/Assets/Project Folder/Scripts/Manager.cs
+++ Candy Cannons/Assets/Project Folder/Scripts/Manager.cs
@@ -1,33 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    [Header("MenuInterface")]
-    #region MenuVariables
-    //Main Menu "Scene"
-    public GameObject mainMenu;
-    //Game "Scene"
-    public GameObject GameScene;
-
-
-    #endregion
-
-    public GameObject playerTank1;
-
+    private GameObject tank1;
+    private GameObject tank2;
+    private List<GameObject> mapObjList;
+    private int currMapIndex;
+    private UIManager uiManager;
+    BulletScript bulletScript;
 // Use this for initialization
-void Start()
+    void Start()
     {
-        
+        uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        mapObjList = GameObject.FindGameObjectsWithTag("Map").ToList();
+        mapObjList[0].SetActive(true);
+        for (int i = 1; i < mapObjList.Count; i++)
+        {
+            mapObjList[i].SetActive(false);
+        }
+        currMapIndex = 0;
+
+        tank1 = GameObject.Find("PlayerTank1");
+        tank2 = GameObject.Find("PlayerTank2");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (tank1.IsDead || tank2.IsDead)
+        //{
+        //    Time.timeScale = 0;
 
+        //    if (tank1.IsDead)
+        //    {
+        //        uiManager.Player1Score++;
+        //    }
+
+        //    if (tank2.IsDead)
+        //    {
+        //        uiManager.Player2Score++;
+        //    }
+        //}
     }
 
-    
-
+    public int CurrMapIndex
+    {
+        get { return currMapIndex; }
+        set { currMapIndex = value; }
+    }
 }
--- Candy Cannons/Assets/Project Folder/Scripts/UIManager.cs
+++ Candy Cannons/Assets/Project Folder/Scripts/UIManager.cs
@@ -17,10 +17,16 @@ public enum currOption_Pause
     EXIT
 }
 
+public enum currOption_Next
+{
+    NEXT = 0,
+    MAIN,
+    EXIT
+}
+
 public class UIManager : MonoBehaviour
 {
     List<GameObject> uiObjects;
-
 
     //------ Main Menu Sprites ------// 
     //assign start sprite
@@ -37,14 +43,33 @@ public class UIManager : MonoBehaviour
     //assign main meny sprite
     private Image mainMenuSprite_Pause;
     //assign exit sprite
-    private Image exitSpite_Pause;
+    private Image exitSprite_Pause;
     //Arrays for storing Resources sprites
     private Sprite[] resumeSpriteList_Pause;
     private Sprite[] mainMenuSpriteList_Pause;
-    private Sprite[] exitSpiteList_Pause;
+    private Sprite[] exitSpriteList_Pause;
+
+    //------ Next Sprites ------//
+    //assign next sprite
+    private Image nextSprite_Next;
+    private Image mainMenuSprite_Next;
+    private Image exitSprite_Next;
+    //Arrays for storing Resources sprites
+    private Sprite[] nextSpriteList_Next;
+    private Sprite[] mainMenuSpriteList_Next;
+    private Sprite[] exitSpriteList_Next;
+
+    //setup player scores
+    private Text player1ScoreText;
+    private Text player2ScoreText;
+
+    private int player1Score;
+    private int player2Score;
 
     private currOption_MainMenu currPointing_MainMenu;
     private currOption_Pause currPointing_Pause;
+    private currOption_Next currPointing_Next;
+
 
     //get Xbox controller
     public XboxController controller;
@@ -63,7 +88,11 @@ public class UIManager : MonoBehaviour
         //------ Load Pause Sprites from Resource ------//
         resumeSpriteList_Pause = Resources.LoadAll<Sprite>("ResumeSprites_Pause");
         mainMenuSpriteList_Pause = Resources.LoadAll<Sprite>("MainMenuSprites_Pause");
-        exitSpiteList_Pause = Resources.LoadAll<Sprite>("ExitSprites_Pause");
+        exitSpriteList_Pause = Resources.LoadAll<Sprite>("ExitSprites_Pause");
+        //------ Load Next Sprites from Resource ------//
+        nextSpriteList_Next = Resources.LoadAll<Sprite>("NextSprites_Next");
+        mainMenuSpriteList_Next = Resources.LoadAll<Sprite>("MainMenuSprites_Next");
+        exitSpriteList_Next = Resources.LoadAll<Sprite>("ExitSprites_Next");
 
         //------ Setup Main Menu's buttons' references ------//
         startSprite_MainMenu = GameObject.Find("StartButton_Main").GetComponent<Image>();
@@ -75,30 +104,46 @@ public class UIManager : MonoBehaviour
         //------ Setup Pause's buttons' reference ------//
         resumeSprite_Pause = GameObject.Find("ResumeButton_Pause").GetComponent<Image>();
         mainMenuSprite_Pause = GameObject.Find("MainMenuButton_Pause").GetComponent<Image>();
-        exitSpite_Pause = GameObject.Find("ExitButton_Pause").GetComponent<Image>();
+        exitSprite_Pause = GameObject.Find("ExitButton_Pause").GetComponent<Image>();
         //initialize which sprite to assign to each option (1 = highlighted sprite, 0 = not highlighted sprite)
         resumeSprite_Pause.sprite = resumeSpriteList_Pause[1];
         mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[0];
-        exitSpite_Pause.sprite = exitSpiteList_Pause[0];
+        exitSprite_Pause.sprite = exitSpriteList_Pause[0];
 
+        //------ Setup Players' scores' reference ------//
+        player1ScoreText = GameObject.Find("Player1Score").GetComponent<Text>();
+        player2ScoreText = GameObject.Find("Player2Score").GetComponent<Text>();
+        player1Score = 0;
+        player2Score = 0;
 
+        //------ Setup Next's buttons' reference ------//
+        nextSprite_Next = GameObject.Find("NextButton_Next").GetComponent<Image>();
+        mainMenuSprite_Next = GameObject.Find("MainMenuButton_Next").GetComponent<Image>();
+        exitSprite_Next = GameObject.Find("ExitButton_Next").GetComponent<Image>();
+        //initialize which sprite to assign to each option (1 = highlighted sprite, 0 = not highlighted sprite)
+        nextSprite_Next.sprite = nextSpriteList_Next[1];
+        mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[0];
+        exitSprite_Next.sprite = exitSpriteList_Next[0];
 
         //set current pointing in Main Menu to START
         currPointing_MainMenu = currOption_MainMenu.START;
-        //set current pointing in Pause to Resume
+        //set current pointing in Pause to RESUME
         currPointing_Pause = currOption_Pause.RESUME;
+        ////set current pointing in Pause to NEXT
+        currPointing_Next = currOption_Next.NEXT;
 
         //------ All UI Elements Loaded, turn off IngameUI and PauseUI ------//
         uiObjects[0].SetActive(true);
         uiObjects[1].SetActive(false);
         uiObjects[2].SetActive(false);
+        uiObjects[3].SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (uiObjects[0].activeInHierarchy) //if currently at Main Menu State
+        if (uiObjects[0].activeInHierarchy) //if currently at MAIN MENU State
         {
             Time.timeScale = 0;
 
@@ -116,6 +161,7 @@ public class UIManager : MonoBehaviour
                     uiObjects[0].SetActive(false);
                     uiObjects[1].SetActive(true);
                     uiObjects[2].SetActive(false);
+                    uiObjects[3].SetActive(false);
                 }
             }
             else if (currPointing_MainMenu == currOption_MainMenu.EXIT)
@@ -134,7 +180,7 @@ public class UIManager : MonoBehaviour
             }
         }
 
-        if (uiObjects[1].activeInHierarchy) //if currently at In Game State
+        if (uiObjects[1].activeInHierarchy) //if currently at INGAME State
         {
             Time.timeScale = 1;
 
@@ -144,10 +190,11 @@ public class UIManager : MonoBehaviour
                 uiObjects[0].SetActive(false);
                 uiObjects[1].SetActive(false);
                 uiObjects[2].SetActive(true);
+                uiObjects[3].SetActive(false);
             }
         }
 
-        if (uiObjects[2].activeInHierarchy) //if currently at In Pause State
+        if (uiObjects[2].activeInHierarchy) //if currently at PAUSE State
         {
             if (currPointing_Pause == currOption_Pause.RESUME)
             {
@@ -157,6 +204,7 @@ public class UIManager : MonoBehaviour
                     uiObjects[0].SetActive(false);
                     uiObjects[1].SetActive(true);
                     uiObjects[2].SetActive(false);
+                    uiObjects[3].SetActive(false);
                 }
 
                 if (XCI.GetButtonDown(XboxButton.DPadUp, controller))
@@ -164,7 +212,7 @@ public class UIManager : MonoBehaviour
                     currPointing_Pause = currOption_Pause.EXIT;
                     resumeSprite_Pause.sprite = resumeSpriteList_Pause[0];
                     mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[0];
-                    exitSpite_Pause.sprite = exitSpiteList_Pause[1];
+                    exitSprite_Pause.sprite = exitSpriteList_Pause[1];
 
                 }
 
@@ -173,7 +221,7 @@ public class UIManager : MonoBehaviour
                     currPointing_Pause = currOption_Pause.MAIN;
                     resumeSprite_Pause.sprite = resumeSpriteList_Pause[0];
                     mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[1];
-                    exitSpite_Pause.sprite = exitSpiteList_Pause[0];
+                    exitSprite_Pause.sprite = exitSpriteList_Pause[0];
                 }
 
             }
@@ -185,6 +233,7 @@ public class UIManager : MonoBehaviour
                     uiObjects[0].SetActive(true);
                     uiObjects[1].SetActive(false);
                     uiObjects[2].SetActive(false);
+                    uiObjects[3].SetActive(false);
                 }
 
                 if (XCI.GetButtonDown(XboxButton.DPadUp, controller))
@@ -192,7 +241,7 @@ public class UIManager : MonoBehaviour
                     currPointing_Pause = currOption_Pause.RESUME;
                     resumeSprite_Pause.sprite = resumeSpriteList_Pause[1];
                     mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[0];
-                    exitSpite_Pause.sprite = exitSpiteList_Pause[0];
+                    exitSprite_Pause.sprite = exitSpriteList_Pause[0];
                 }
 
                 if (XCI.GetButtonDown(XboxButton.DPadDown, controller))
@@ -200,7 +249,7 @@ public class UIManager : MonoBehaviour
                     currPointing_Pause = currOption_Pause.EXIT;
                     resumeSprite_Pause.sprite = resumeSpriteList_Pause[0];
                     mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[0];
-                    exitSpite_Pause.sprite = exitSpiteList_Pause[1];
+                    exitSprite_Pause.sprite = exitSpriteList_Pause[1];
                 }
             }
             else if (currPointing_Pause == currOption_Pause.EXIT)
@@ -215,7 +264,7 @@ public class UIManager : MonoBehaviour
                     currPointing_Pause = currOption_Pause.MAIN;
                     resumeSprite_Pause.sprite = resumeSpriteList_Pause[0];
                     mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[1];
-                    exitSpite_Pause.sprite = exitSpiteList_Pause[0];
+                    exitSprite_Pause.sprite = exitSpriteList_Pause[0];
                 }
 
                 if (XCI.GetButtonDown(XboxButton.DPadDown, controller))
@@ -223,7 +272,92 @@ public class UIManager : MonoBehaviour
                     currPointing_Pause = currOption_Pause.RESUME;
                     resumeSprite_Pause.sprite = resumeSpriteList_Pause[1];
                     mainMenuSprite_Pause.sprite = mainMenuSpriteList_Pause[0];
-                    exitSpite_Pause.sprite = exitSpiteList_Pause[0];
+                    exitSprite_Pause.sprite = exitSpriteList_Pause[0];
+                }
+            }
+        }
+
+        if (uiObjects[3].activeInHierarchy) //if currently at In NEXT State
+        {
+            if (currPointing_Next == currOption_Next.NEXT)
+            {
+                if (XCI.GetButtonDown(XboxButton.B, controller))
+                {
+                    Time.timeScale = 1;
+                    uiObjects[0].SetActive(false);
+                    uiObjects[1].SetActive(true);
+                    uiObjects[2].SetActive(false);
+                    uiObjects[3].SetActive(false);
+                    //turn off current map
+                    //launch next map
+                }
+
+                if (XCI.GetButtonDown(XboxButton.DPadUp, controller))
+                {
+                    currPointing_Next = currOption_Next.EXIT;
+                    nextSprite_Next.sprite = nextSpriteList_Next[0];
+                    mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[0];
+                    exitSprite_Next.sprite = exitSpriteList_Next[1];
+
+                }
+
+                if (XCI.GetButtonDown(XboxButton.DPadDown, controller))
+                {
+                    currPointing_Next = currOption_Next.MAIN;
+                    nextSprite_Next.sprite = nextSpriteList_Next[0];
+                    mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[1];
+                    exitSprite_Next.sprite = exitSpriteList_Next[0];
+                }
+
+            }
+            else if (currPointing_Next == currOption_Next.MAIN)
+            {
+                if (XCI.GetButtonDown(XboxButton.B, controller))
+                {
+                    Time.timeScale = 0;
+                    uiObjects[0].SetActive(true);
+                    uiObjects[1].SetActive(false);
+                    uiObjects[2].SetActive(false);
+                    uiObjects[3].SetActive(false);
+                }
+
+                if (XCI.GetButtonDown(XboxButton.DPadUp, controller))
+                {
+                    currPointing_Next = currOption_Next.NEXT;
+                    nextSprite_Next.sprite = nextSpriteList_Next[1];
+                    mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[0];
+                    exitSprite_Next.sprite = exitSpriteList_Next[0];
+                }
+
+                if (XCI.GetButtonDown(XboxButton.DPadDown, controller))
+                {
+                    currPointing_Next = currOption_Next.EXIT;
+                    nextSprite_Next.sprite = nextSpriteList_Next[0];
+                    mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[0];
+                    exitSprite_Next.sprite = exitSpriteList_Next[1];
+                }
+            }
+            else if (currPointing_Next == currOption_Next.EXIT)
+            {
+                if (XCI.GetButtonDown(XboxButton.B, controller))
+                {
+                    Application.Quit();
+                }
+
+                if (XCI.GetButtonDown(XboxButton.DPadUp, controller))
+                {
+                    currPointing_Next = currOption_Next.MAIN;
+                    nextSprite_Next.sprite = nextSpriteList_Next[0];
+                    mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[1];
+                    exitSprite_Next.sprite = exitSpriteList_Next[0];
+                }
+
+                if (XCI.GetButtonDown(XboxButton.DPadDown, controller))
+                {
+                    currPointing_Next = currOption_Next.NEXT;
+                    nextSprite_Next.sprite = nextSpriteList_Next[1];
+                    mainMenuSprite_Next.sprite = mainMenuSpriteList_Next[0];
+                    exitSprite_Next.sprite = exitSpriteList_Next[0];
                 }
             }
         }
@@ -235,8 +369,29 @@ public class UIManager : MonoBehaviour
         GameObject mainMenuUI = GameObject.Find("MainMenuUI");
         GameObject ingameUI = GameObject.Find("IngameUI");
         GameObject pauseUI = GameObject.Find("PauseUI");
+        GameObject nextUI = GameObject.Find("NextUI");
         uiObjects.Add(mainMenuUI);
         uiObjects.Add(ingameUI);
         uiObjects.Add(pauseUI);
+        uiObjects.Add(nextUI);
+    }
+
+    //Getter & Setter for both player's scores
+    public int Player1Score
+    {
+        get { return player1Score; }
+        set { player1Score = value; }
+    }
+
+    public int Player2Score
+    {
+        get { return player2Score; }
+        set { player2Score = value; }
+    }
+
+    public List<GameObject> UIObjects
+    {
+        get { return uiObjects; }
+        set { uiObjects = value; }
     }
 }
